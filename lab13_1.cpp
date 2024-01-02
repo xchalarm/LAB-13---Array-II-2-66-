233150436fbcade52c94e1923784be79444bcde7
xchalarm <120165210+xchalarm@users.noreply.github.com>
2024-01-02 14:05:53 +0700

    Initial commit

---
+++ lab13_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void stat(const double[],int,double[]);
+
+int main(){
+    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
+    int N = sizeof(A)/sizeof(A[0]);
+    double B[6];
+    stat(A,N,B);
+    cout << "Arithmetic Mean = " << B[0];
+    cout << "\nStandard Deviation = " << B[1];
+    cout << "\nGeometric Mean = " << B[2];
+    cout << "\nHarmonic Mean = " << B[3];
+    cout << "\nMax = " << B[4];
+    cout << "\nMin = " << B[5];
+    return 0;
+}
